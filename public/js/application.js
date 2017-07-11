24a7631925bb2065b34e4edadf642eb84b99afb1
codeamac19 <codeamac19@gmail.com>
2017-07-11 17:02:17 -0500

    Visualizar en inspector tecla oprimida

--- public/js/application.js
+++ public/js/application.js
@@ -11,30 +11,34 @@ $(document).ready(function(){
         //en 950 milisegs, en la fila encontrar hijo con lcase "active" y al siguiente elemento y añadir la clase "active"
       };
     });
+
   //crear funcion que recibe un jugador
   function lanzar_dado(player) {
     //-asignar variable que obtiene objeto con la clase active = CURRENT
-    var $current_player = $(player).find(".active");
-    //-asignar variable que obtiene el siguiente objeto a la clase active = NEXT
-    var $next_player = $current_player.next();
     //-remover la clase "active" de CURRENT
-    $current_player.removeClass("active");
+    var $current_player = $(player).find(".active").removeClass("active");
+    //-asignar variable que obtiene el siguiente objeto a la clase active = NEXT
     //-agregar clase "active" a NEXT
-    $next_player.addClass("active")
-
+    $current_player.next().addClass("active");
 
     //si el largo del <tr id="Player1">  es mayor o igual la indice del <td class="active">
     if (player.length + 1 >= $($current_player).index() ) {
       //establecer tiempo de ejecucion de lanzar el dado
       setTimeout(function() {
         lanzar_dado(player);
-      },1000);
+      },500);
     }
-  }
+  };
 
 
   lanzar_dado("#Player1");
   lanzar_dado("#Player2");
 
+  //source: https://api.jquery.com/event.which/
+  //en la ventana al aprentar tecla..
+  $(window).keydown(function( event ) {
+    //mostrar el tipo de evento y tecla apretada
+    console.log(event.type+": "+ event.which);
+  });
 
 });
